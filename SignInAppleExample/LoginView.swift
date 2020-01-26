3294dfe5ca65c426ee4f991788b10802e98d6d21
Renato Lopes <renattoolopes@gmail.com>
2020-01-26 14:10:26 -0400

    Rename ContentView to LoginView

--- SignInAppleExample/LoginView.swift
+++ SignInAppleExample/LoginView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LoginView.swift
 //  SignInAppleExample
 //
 //  Created by Renato Lopes on 24/01/20.
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LoginView: View {
     var body: some View {
         Text("Hello, World!")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LoginView()
     }
 }
