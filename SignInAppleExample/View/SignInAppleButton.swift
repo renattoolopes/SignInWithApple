7bb677633cc55e05a08521d61723605b9de905c2
Renato Lopes <renattoolopes@gmail.com>
2020-01-26 14:14:37 -0400

    Created sign in apple button

---
+++ SignInAppleExample/View/SignInAppleButton.swift
@@ -0,0 +1,20 @@
+//
+//  SignInAppleButton.swift
+//  SignInAppleExample
+//
+//  Created by Renato Lopes on 26/01/20.
+//  Copyright © 2020 renato. All rights reserved.
+//
+
+import SwiftUI
+import AuthenticationServices
+
+final class SignInAppleButton: UIViewRepresentable {
+    func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
+        return ASAuthorizationAppleIDButton()
+    }
+    
+    func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: UIViewRepresentableContext<SignInAppleButton>) {
+        
+    }
+}
